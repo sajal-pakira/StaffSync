eab0176678e2a161621b29d6a249437eaea8e44b
Sajal Pakira <uniquesajal6@gmail.com>
2026-01-05 20:43:53 +0530

    Auth validation done & loggedin user role is assigned

--- src/App.jsx
+++ src/App.jsx
@@ -16,19 +16,27 @@ const App = () => {
   }, []);
 
   const [user, setUser] = useState(null);
+  const authData = useContext(AuthContext);
 
   const handleLogin = (email, password) => {
     if (email == "admin@me.com" && password == "123") {
       setUser("admin");
-    } else if (email == "user@me.com" && password == "123") {
+      localStorage.setItem("loggedInUser", JSON.stringify({ role: "admin" }));
+    } else if (
+      authData &&
+      authData.employeeData.find(
+        (e) => e.email == email && e.password == password
+      )
+    ) {
       setUser("employee");
+      localStorage.setItem(
+        "loggedInUser",
+        JSON.stringify({ role: "employee" })
+      );
     } else {
       alert("Invalid Credentials!!");
     }
   };
-
-  const data = useContext(AuthContext);
-  console.log(data);
 
   return (
     <>
