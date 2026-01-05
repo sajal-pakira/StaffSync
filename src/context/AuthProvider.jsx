069978d2b361216a3ef522b87f5c4f2480e921ea
Sajal Pakira <uniquesajal6@gmail.com>
2026-01-05 18:47:13 +0530

    AuthContext created

--- src/context/AuthProvider.jsx
+++ src/context/AuthProvider.jsx
@@ -1,7 +1,14 @@
 import PropTypes from "prop-types";
+import { createContext } from "react";
+
+export const AuthContext = createContext();
 
 const AuthProvider = ({ children }) => {
-  return <div>{children}</div>;
+  return (
+    <div>
+      <AuthContext.Provider value={"sajal"}>{children}</AuthContext.Provider>
+    </div>
+  );
 };
 
 AuthProvider.propTypes = {
